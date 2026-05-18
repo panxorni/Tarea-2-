324d5fae47386a3a826d4d6d0ac596dd1e5c6570
Matías Vicente Pareja Escalona <matyparejaescalona@gmail.com>
2026-05-17 20:27:39 -0400

    Agrego ahora todo sobre InvitadoExterno

--- src/main/java/org/example/InvitadoExterno.java
+++ src/main/java/org/example/InvitadoExterno.java
@@ -1,4 +1,48 @@
 package org.example;
 
-public class InvitadoExterno {
+public class InvitadoExterno implements Invitable{
+    private String nombreCompleto;
+    private String correoElectronico;
+    public InvitadoExterno (String nombreCompleto, String correoElectronico){
+        if (nombreCompleto==null || nombreCompleto.isBlank()){
+            throw new IllegalArgumentException("el nombre no puede ser vacio");
+        }
+        this.nombreCompleto=nombreCompleto;
+        if (correoElectronico==null || correoElectronico.isBlank()){
+            throw new IllegalArgumentException("el correo no puede ser vacio");
+        }
+        this.correoElectronico=correoElectronico;
+    }
+    public String getNombreCompleto(){
+        return nombreCompleto;
+    }
+    public void setNombreCompleto(String nombreCompleto){
+        if (nombreCompleto==null || nombreCompleto.isBlank()){
+            throw new IllegalArgumentException("no puede estar vacio el nombre");
+        }
+        this.nombreCompleto = nombreCompleto;
+    }
+    public String getCorreoElectronico(){
+        return correoElectronico;
+    }
+    public void setCorreoElectronico(String correoElectronico){
+        if (correoElectronico==null || correoElectronico.isBlank()){
+            throw new IllegalArgumentException("no puede estar vacio el correo");
+        }
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String nombreInvitable() {
+        return nombreCompleto;
+    }
+    public String correoInvitable() {
+        return correoElectronico;
+    }
+    public void invitar() {
+        System.out.println("Felicidades, " + nombreCompleto + " ha recibido una invitación");
+    }
+    public String toString(){
+        return "nombre: " + nombreCompleto + ", correo: " + correoElectronico;
+    }
+
 }
